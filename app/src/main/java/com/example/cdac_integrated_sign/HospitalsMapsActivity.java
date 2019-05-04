7a03adb7f4ec7270f0ff69b56ff94fe2bec1deef
Ulugbek <ulugbek23@mail.ru>
2019-05-04 22:49:51 +0530

    04.05.2019

--- app/src/main/java/com/example/cdac_integrated_sign/HospitalsMapsActivity.java
+++ app/src/main/java/com/example/cdac_integrated_sign/HospitalsMapsActivity.java
@@ -1,6 +1,12 @@
 package com.example.cdac_integrated_sign;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -34,6 +40,8 @@ public class HospitalsMapsActivity extends FragmentActivity implements OnMapRead
     private GoogleMap mMap;
     String response;
     LatLng myLocation;
+    LatLng myPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,17 +49,52 @@ public class HospitalsMapsActivity extends FragmentActivity implements OnMapRead
 
         Intent intent = getIntent();
         response = intent.getStringExtra("response");
-        Double my_latitude = intent.getDoubleExtra("my_latitude",0);
-        Double my_longitude = intent.getDoubleExtra("my_longitude",0);
-        myLocation = new LatLng(my_latitude,my_longitude);
+        Double my_latitude = intent.getDoubleExtra("my_latitude", 0);
+        Double my_longitude = intent.getDoubleExtra("my_longitude", 0);
+        myLocation = new LatLng(my_latitude, my_longitude);
 
-        Toast.makeText(this, "come 1"+response, Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,  my_latitude+" "+my_longitude, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "come 1"+response, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,  my_latitude+" "+my_longitude, Toast.LENGTH_SHORT).show();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Getting LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Getting the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        // Getting Current Location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null) {
+            // Getting latitude of the current location
+            double latitude = location.getLatitude();
+
+            // Getting longitude of the current location
+            double longitude = location.getLongitude();
+
+            // Creating a LatLng object for the current location
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            myPosition = new LatLng(latitude, longitude);
+        }
     }
 
     /**
@@ -66,10 +109,10 @@ public class HospitalsMapsActivity extends FragmentActivity implements OnMapRead
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Marker marker=null;
+        Marker marker = null;
         try {
             JSONArray jsonArray = new JSONArray(response); // set global variable
-            for(int i=0; i<jsonArray.length(); i++){
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 LatLng hospital = new LatLng(jsonObject.getDouble("latitude"), jsonObject.getDouble("longitude"));
                 marker = mMap.addMarker(new MarkerOptions().position(hospital).title(jsonObject.getString("name")));
@@ -80,7 +123,19 @@ public class HospitalsMapsActivity extends FragmentActivity implements OnMapRead
         }
 
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(hospital));
-        if(marker!=null) mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 8));
+        if (marker != null)
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 8));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if(myPosition!=null) mMap.addMarker(new MarkerOptions().position(myPosition).title("My position"));
+        mMap.setMyLocationEnabled(true);
     }
-
 }
